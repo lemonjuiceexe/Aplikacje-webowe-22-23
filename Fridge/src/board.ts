b630340f2255a7c9df207ecee4cf51bd43ec865c
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2022-11-14 21:02:58 +0100

    Fridge: Add TinyMCE editor

--- Fridge/src/board.ts
+++ Fridge/src/board.ts
@@ -1,3 +1,4 @@
+import tinymce from "tinymce";
 import "./css/board.css";
 import { Note } from "./note";
 
@@ -26,16 +27,47 @@ export class Board{
     
     /*Public methods */
     public addNote(title: string, content: string){
-        // position = this.defaultNotePosition;
-        const position = {...this.defaultNotePosition};
-        const size = {...this.defaultNoteSize};
-        
+        /* Setup the note HTML element */
         const noteElement = this.createNoteElement(this.allCount, title, content);
         noteElement.style.left = this.defaultNotePosition.x + "px";
         noteElement.style.top = this.defaultNotePosition.y + "px";
+        
+        noteElement.querySelector(".note-content")!.addEventListener("mousedown", (e) => {
+            let textArea = document.createElement("textarea");
+            // textArea.style.display = "none";
+            this.wrapper.appendChild(textArea);
+            tinymce.init({
+                selector: "textarea"
+            }).then(() => {
+                // Replace tinymce's bottom bar with a custom one
+                let tinymceBottomRightBar = document.querySelector(".tox-statusbar__branding") as HTMLSpanElement;
+                tinymceBottomRightBar.innerHTML = "";
+                tinymceBottomRightBar.appendChild(this.createCustomTinymceBar());
+                
+                let closeEditor = () => {
+                    tinymce.remove();
+                    textArea.remove();
+                }
+
+                tinymceBottomRightBar.querySelector(".tinymce-button-exit")!.addEventListener("click", closeEditor);
+                tinymceBottomRightBar.querySelector(".tinymce-button-save")!.addEventListener("click", () => {
+                    content = tinymce.activeEditor!.getContent();
+                    noteElement.querySelector(".note-content")!.innerHTML = content;
+                    closeEditor();
+                });
+
+                tinymce.activeEditor!.setContent(content);
+                tinymce.activeEditor!.focus();
+            });
+            
+        });
+
         this.wrapper.appendChild(noteElement);
         
         // Logic
+        const position = {...this.defaultNotePosition};
+        const size = {...this.defaultNoteSize};
+
         let note = new Note(this, noteElement, this.allCount, title, position, size, content);
         this.notes.push(note);
         this.updateCounters(Boolean(1));
@@ -60,7 +92,7 @@ export class Board{
     }
     private createNoteElement(id: number, title: string, content: string){
         // Create note HTML element structure
-        const noteElement = document.createElement("div");
+        let noteElement = document.createElement("div");
         noteElement.classList.add("note");
         noteElement.id = "note-" + id;
         
@@ -74,7 +106,7 @@ export class Board{
         
         let noteCloseElement = document.createElement("div");
         noteCloseElement.classList.add("note-close");
-        noteCloseElement.innerText = "X";
+        noteCloseElement.innerHTML = "&#10006;";
         dragzone.appendChild(noteCloseElement);
         
         let noteContentElement = document.createElement("p");
@@ -84,5 +116,23 @@ export class Board{
     
     
         return noteElement;
+    }
+    private createCustomTinymceBar(){
+        // Create custom bar html structure
+        let customBar = document.createElement("span");
+
+        let exitButton = document.createElement("button");
+        exitButton.innerText = "Exit";
+        exitButton.classList.add("tinymce-button-exit");
+        
+        let saveButton = document.createElement("button");
+        saveButton.innerText = "Save";
+        saveButton.classList.add("tinymce-button-save");
+
+        customBar.appendChild(exitButton);
+        customBar.appendChild(saveButton);
+
+
+        return customBar;
     }
 }
--- Fridge/src/boardIndex.ts
+++ Fridge/src/boardIndex.ts
@@ -5,8 +5,6 @@ const addNoteButton = wrapper.querySelector(".spawn-button") as HTMLDivElement;
 const counterAll = document.querySelector(".counter-all > span") as HTMLSpanElement;
 const counterActive = document.querySelector(".counter-current > span") as HTMLSpanElement;
 
-console.log("ahjs");
-
 const board = new Board(wrapper, counterAll, counterActive);
 
 addNoteButton.addEventListener("click", board.addNote.bind(board, "Tytuł notatki", "Treść notatki"));
