a9b589ac12e03030e2e1f88af5976e1ab632fe0c
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2022-11-20 19:46:08 +0100

    Fridge: for testing: fix using boardname input

--- Fridge/src/index.ts
+++ Fridge/src/index.ts
@@ -4,4 +4,9 @@ let wrapper = document.querySelector(".wrapper")!;
 let boardNameInput: HTMLInputElement = wrapper.querySelector("input")!;
 boardNameInput.setAttribute("size", boardNameInput.getAttribute("placeholder")!.length.toString());
 
-sessionStorage.setItem("boardName", boardNameInput.value);
+// sessionStorage.setItem("boardName", boardNameInput.value);
+
+const boardEnter = () => {
+    sessionStorage.setItem("boardName", boardNameInput.value);
+    window.location.href='boardIndex.html';
+};
